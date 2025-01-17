476ea214b351d51a02713c1fdab01c5abb211826
gSaidL <guilhermefracasso08@gmail.com>
2025-01-17 17:30:09 -0300

    Base da interface

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -1,4 +1,30 @@
 package View;
 
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private JPanel contentPane;
+    private JTextField textCpf;
+    private JLabel labelCpf;
+    private JLabel labelResultado;
+    private JLabel textResultado;
+    private JButton buttonConfirmar;
+
+    public String getTextCpf() {
+        return textCpf.getText();
+    }
+
+    public MainWindow(){
+        setTitle("Conversor de CPF");
+        setSize(400,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(contentPane);
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
